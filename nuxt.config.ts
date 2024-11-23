8b9ceebb3b866c9bdc7d5900853c86c6da1d24b8
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-11-23 21:20:53 +0700

    Setting buildAssetsDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
     devtools: {enabled: false},
     css: ['@/assets/css/main.css'],
     ssr: false,
+    app: {
+        buildAssetsDir: '/_nuxt/',
+    },
     postcss: {
         plugins: {
             tailwindcss: {},
