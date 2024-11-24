f4305d08b8a3032ab8967b85bd781a502c9f05a6
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-11-24 11:59:11 +0700

    Resize quote images

--- components/OutStory.vue
+++ components/OutStory.vue
@@ -18,24 +18,24 @@ export default {
             quotes: [
                 {
                     text: "Every step we took brought us closer to this day.",
-                    image: "/images/HNG00140.jpg",
+                    image: "/images/quotes/HNG00140.jpg",
                 },
                 {
                     text: "Love is not about finding someone to live with, it's about finding someone you can't live without.",
-                    image: "/images/HNG00416_1.jpg",
+                    image: "/images/quotes/HNG00416_1.jpg",
                 },
                 {
                     text: "I saw that you were perfect, and so I loved you. \n" +
                         "Then I saw that you were not perfect and I loved you even more.",
-                    image: "/images/HNG09883.jpg",
+                    image: "/images/quotes/HNG09883.jpg",
                 },
                 {
                     text: "Two souls, one journey, a love that grows stronger with each chapter, capturing moments we’ll cherish forever.",
-                    image: "/images/HNG00177.jpg",
+                    image: "/images/quotes/HNG00177.jpg",
                 },
                 {
                     text: "Today, our hearts unite, and from here, we walk together—writing a love story that will last a lifetime.",
-                    image: "/images/HNG00276.jpg",
+                    image: "/images/quotes/HNG00276.jpg",
                 },
             ],
         };
